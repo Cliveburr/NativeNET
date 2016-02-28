6538901c299df46cd04bc908fc6ce827f58abda9
Cliveburr <cliveburr@gmail.com>
2016-02-28 12:47:36 -0300

    Change WriteFile to IO and create Read method

---
+++ NativeNET/Kernel32/File/IO.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Runtime.InteropServices;
+using System.Threading;
+
+namespace NativeNET.Kernel32.File
+{
+    public static class IO
+    {
+        [DllImport("kernel32.dll", EntryPoint = "WriteFile", SetLastError = true)]
+        static internal extern bool WriteFileMethod(IntPtr hFile, byte[] lpBuffer, uint nNumberOfBytesToWrite, out uint lpNumberOfBytesWritten, [In] ref NativeOverlapped lpOverlapped);
+
+        [DllImport("kernel32.dll", EntryPoint = "ReadFile", SetLastError = true)]
+        static internal extern bool ReadFileMethod(IntPtr hFile, [Out] byte[] lpBuffer, uint nNumberOfBytesToRead, out uint lpNumberOfBytesRead, [In] ref NativeOverlapped lpOverlapped);
+
+        public static bool Write(IntPtr file, byte[] buffer, uint numberOfBytesToWrite, out uint numberOfBytesWritten, ref NativeOverlapped overlapped)
+        {
+            return WriteFileMethod(file, buffer, numberOfBytesToWrite, out numberOfBytesWritten, ref overlapped);
+        }
+
+        public static bool Read(IntPtr file, byte[] buffer, uint nNumberOfBytesToRead, out uint lpNumberOfBytesRead, ref NativeOverlapped overlapped)
+        {
+            return ReadFileMethod(file, buffer, nNumberOfBytesToRead, out lpNumberOfBytesRead, ref overlapped);
+        }
+    }
+}
